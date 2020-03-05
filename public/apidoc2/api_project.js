e2009153ba555e66c2694305a409e521e0eabc13
yuichiro6147 <tak6147@gmail.com>
2020-03-05 14:45:40 +0900

    別階層にもapidocを作成

---
+++ public/apidoc2/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "sample apidoc",
+  "version": "1.0.0",
+  "description": "サンプルAPI",
+  "url": "http://localhost:8080/api",
+  "sampleUrl": "http://localhost:8080/api",
+  "order": [],
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-03-05T05:42:31.263Z",
+    "url": "http://apidocjs.com",
+    "version": "0.20.0"
+  }
+});
