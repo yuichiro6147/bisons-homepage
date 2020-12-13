461dc5fc03413b61cc89d5497dc41d1e42867771
Yuichiro Takeda <tak6147@gmail.com>
2020-12-13 19:03:27 +0900

    ヘッダをコンポーネント化

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <title>D-BISONS</title>
-    <meta name="description" content="D-BISONSのホームページ">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSS -->
-    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-</head>
-
+@include('header')
 <body>
 <div id="home" class="big-bg">
     <header class="page-header wrapper">
