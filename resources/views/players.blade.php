1eb851c83a82feca463299938a982091e692d8de
Yuichiro Takeda <tak6147@gmail.com>
2020-12-13 19:03:45 +0900

    選手紹介ページを作成

---
+++ resources/views/players.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+@include('header')
+<body>
+<div class="big-bg">
+    <header class="page-header wrapper">
+        <nav>
+            <ul class="main-nav">
+                <li><a href="#">About</a></li>
+                <li><a href="#">選手紹介</a></li>
+                <li><a href="#">試合結果</a></li>
+                <li><a href="#">個人成績</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="players-content wrapper">
+        <h2 class="page-title">選手紹介</h2>
+    </div>
+</div>
+
+<div class="wrapper grid">
+    <div class="item">
+    </div>
+    <div class="item">
+    </div>
+    <div class="item">
+    </div>
+</div>
+
+<footer>
+    <div class="wrapper">
+        <p><small>&copy; 2020 D-BISONS</small></p>
+    </div>
+</footer>
+</body>
+</html>
