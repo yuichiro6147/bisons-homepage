d9e354f63073ca1e090674c868b49b975a1f52bd
Yuichiro Takeda <tak6147@gmail.com>
2020-12-19 14:49:35 +0900

    トップページから選手紹介画面に遷移できるようにした

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
         <nav>
             <ul class="main-nav">
                 <li><a href="#">About</a></li>
-                <li><a href="#">選手紹介</a></li>
+                <li><a href="/players">選手紹介</a></li>
                 <li><a href="#">試合結果</a></li>
                 <li><a href="#">個人成績</a></li>
             </ul>
